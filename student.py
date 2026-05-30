45eb88c85d03f0054d872d4160ccf80842b0beed
CodeWithPatan <patansohalkhan@gmail.com>
2026-05-30 18:29:00 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+print("Student name is", name)
+branch=input("Enter your branch: ")
+print(name, "belongs to",branch)
+college=input("Enter your college: ")
+print(name, "studying in", college,"and belongs to",branch,"branch")
+
